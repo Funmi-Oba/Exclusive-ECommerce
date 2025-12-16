4c7108df520489b8f163d63a7e66835702d5b6a9
Funmilayo Oba <113449697+Funmi-Oba@users.noreply.github.com>
2025-12-16 15:25:22 +0100

    second commit

--- src/pages/Checkout.vue
+++ src/pages/Checkout.vue
@@ -1,8 +1,8 @@
 <template lang="">
   <div class="w-full">
-    <h2 class="mt-20 mb-10 text-3xl font-semibold tracking-wider">Billing Details</h2>
-    <div class="flex items-center">
+    <div class="flex">
       <div class="w-1/2">
+        <h2 class="mt-20 mb-10 text-3xl font-semibold tracking-wider">Billing Details</h2>
         <form class="flex flex-col justify-center w-3/4 space-y-2">
           <label for="" class="text-lg">First Name<span class="text-red-600">*</span></label>
           <input type="text" name="" class="p-2 mb-8 bg-gray-200" />
@@ -20,7 +20,8 @@
           <input type="email" name="" class="p-2 mb-8 bg-gray-200" />
         </form>
       </div>
-      <div class="w-1/2 p-6 space-y-8">
+      <div class="w-1/2 px-6 space-y-8">
+            <h2 class="mt-20 mb-10 text-3xl font-semibold tracking-wider">Order summary</h2>
         <div v-for="item in cart" :key="item.id" class="flex items-center justify-between">
           <div class="flex items-center gap-4">
             <img :src="item.image" class="object-contain w-16 h-16" />
@@ -58,24 +59,20 @@
         </div>
         <div class="flex gap-2">
           <input type="text" placeholder="Coupon Code" class="flex-1 p-2 border rounded" />
-           <button class="px-4 py-2 text-white bg-[#DB4444] rounded">
-            Apply Coupon
-          </button>
+          <button class="px-4 py-2 text-white bg-[#DB4444] rounded">Apply Coupon</button>
         </div>
-        <button class=" rounded px-4 py-2 text-white bg-[#DB4444]">
-         Place Order
-          </button>
+        <button class="rounded px-4 py-2 text-white bg-[#DB4444]">Place Order</button>
       </div>
     </div>
   </div>
 </template>
 <script>
-import RedButton from '@/components/RedButton.vue';
+import RedButton from '@/components/RedButton.vue'
 import { mapGetters } from 'vuex'
 export default {
-    components:{
-     RedButton   
-    },
+  components: {
+    RedButton,
+  },
   computed: {
     ...mapGetters('cart', {
       cart: 'getCart',
